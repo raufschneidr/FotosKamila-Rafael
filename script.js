e5129a82594ba87cf1344187c96db047df4d8e97
raufschneidr <raufschneidr@gmail.com>
2026-01-08 17:30:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("JS carregado");
+let indexAtual = 0;
+let fotos = [];
+
+fetch("data/fotos.json")
+  .then(res => res.json())
+  .then(dados => {
+    fotos = dados;
+    criarSlides();
+    atualizarCarrossel();
+  });
+
+function criarSlides() {
+  const track = document.querySelector(".carousel-track");
+  track.innerHTML = "";
+
+  fotos.forEach(foto => {
+    const img = document.createElement("img");
+    img.src = `media/fotos/${foto.arquivo}`;
+    track.appendChild(img);
+  });
+}
+
+function atualizarCarrossel() {
+  const track = document.querySelector(".carousel-track");
+  const largura = 600;
+  track.style.transform = `translateX(-${indexAtual * largura}px)`;
+}
